fadc46221c66d92713decc7615e7b81a3ba7a8e6
TollscheinAle <a.tollschein@gmx.at>
2023-04-02 23:11:54 +0200

    Anlegen der Datenbank

---
+++ src/main/java/Tollschein/analyseTest/AnalyseTestApplication.java
@@ -0,0 +1,13 @@
+package Tollschein.analyseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tollschein/analyseTest/AnalyseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Tollschein.analyseTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
